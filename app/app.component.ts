e59340f6ff5855d42aa1e1f95b33515f9cef9ddd
chisun-joung <csjoung@gmail.com>
2016-12-15 23:01:33 +0900

    style routerlink and routerLinkActiveOption

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  styles: [`
+a{
+  text-decoration: none;
+}
+
+a.active{
+  font-weight: bold;
+}
+`],
   template: `
 <nav>
-  <a routerLink="">Home</a>
-  <a routerLink="contacts">Contacts</a>
+  <a 
+    routerLink="" 
+    routerLinkActive="active"
+    [routerLinkActiveOptions]="{exact:true}"
+    >Home</a>
+  <a routerLink="contacts" routerLinkActive="active" >Contacts</a>
 </nav>
 <router-outlet></router-outlet>
 `
