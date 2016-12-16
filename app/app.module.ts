9d9a8be1f1b6debeefb6853f58bc5b1fb35e4feb
chisun-joung <csjoung@gmail.com>
2016-12-16 15:06:32 +0900

    Load Data Based on Aagular 2 Route Rarams

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,11 @@ import {NgModule}      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import appRoutes from './app.routes';
-import {APP_BASE_HREF} from "@angular/common";
 
 
 @NgModule({
   imports:      [ BrowserModule, appRoutes ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [{provide: APP_BASE_HREF, useValue:'/'}]
 })
 export class AppModule { }
